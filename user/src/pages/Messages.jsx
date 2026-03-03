cceee769e82b99e34aba55fb51a326ffff599b84
Srihari2617 <sriharibv.24aid@kongu.edu>
2026-03-03 21:42:30 +0530

    working - myListingPage

--- user/src/pages/Messages.jsx
+++ user/src/pages/Messages.jsx
@@ -1,13 +1,23 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import { dummyChats } from '../assets/assets';
 import { MessageCircle, Search } from 'lucide-react';
-
+import {format,isToday,isYesterday,parseISO } from 'date-fns'
+import { useDispatch } from 'react-redux';
+import { setChat } from '../app/features/chatSlice';
 
 const Messages = () => {
+
+  const dispatch=useDispatch();
+
+  const user={id:"user_1"};
 
   const [chats,setChats] =useState([]);
   const [searchQuery,setSearchQuery] =useState("");
   const [loading,setLoading] =useState(true);
+
+  const handleOpenChat =(chat)=>{
+    dispatch(setChat({listing:chat.listing,chatId:chat.id}))
+  }
 
   const fetchChats =async ()=>{
     setChats(dummyChats);
@@ -23,7 +33,34 @@ const Messages = () => {
     return()=>clearInterval(interval);
   },[])
 
+  const formatTime=(dateString)=>{
+    if(!dateString) return;
 
+    const date=parseISO(dateString);
+
+    if(isToday(date)){
+      return 'Today '+format(date,"HH:mm");
+    }
+
+    if(isYesterday(date)){
+      return 'Yestarday '+format(date,"HH:mm");
+    }
+
+    return format(date,"MMM d");
+
+  }
+
+  const filteredChats =useMemo(()=>{
+    const query=searchQuery.toLowerCase();
+    return chats.filter((chat)=>{
+      const chatUser=chat.chatUserId===user?.id ? chat?.ownerUser : chat.chatUser;
+
+       return chat.listing?.title?.toLowerCase().includes(query) || 
+      chatUser?.name?.toLowerCase().includes(query);
+    
+    })
+
+  },[chats,searchQuery])
 
   return (
     <div className='mx-auto min-h-screen px-6 md:px-16 lg:px-24 xl:px-32'>
@@ -47,14 +84,52 @@ const Messages = () => {
             <div className='text-center text-gray-500 py-20'>
               Loading messages.....
             </div>
-          ): chats.length===0?(
+          ): filteredChats.length===0?(
             <div className='bg-white p-14 border border-gray-200 rounded-2xl shadow-xs text-center'>
               <div className='w-16 h-16 flex items-center justify-center rounded-full bg-gray-100 mx-auto mb-4'>
                   <MessageCircle className='w-8 h-8 text-gray-400'/> 
               </div>
               <h3 className='text-xl font-medium mb-3 text-gray-800'>{searchQuery?'No Chats Found':'No Messages Yet'}</h3>
+              <p className='text-gray-600'>
+                {searchQuery?" Try different search":" start the conversation with seller or buyer"}
+              </p>
             </div>
-          ):()}
+          ):(
+            <div className='bg-white rounded-lg shadow-xs border border-gray-200 divide-y divide-gray-200'>
+                {
+                  filteredChats.map((chat)=>{
+                    const chatUser=chat.chatUserId===user?.id? chat.ownerUser : chat.chatUser;
+
+                    return (
+                      <button key={chat.id} onClick={()=> handleOpenChat(chat)} className='w-full p-4 mb-3 hover:bg-gray-50 transition-colors
+                       text-left'>
+                        <div className='flex items-start space-x-4'>
+
+                          <div className='flex-shrink-0'>
+                            <img src={chatUser?.image} alt={chat?.chatUser?.name} 
+                              className='w-12 h-12 rounded-lg object-cover'/>
+                          </div>
+
+                          <div className='flex-1 min-w-0'>
+                              <div className='flex items-center justify-between mb-1'>
+                                  <h3 className='font-semibold text-gray-8 00 truncate'>{chat?.listing?.title}</h3>
+                                  <span className='text-xs text-gray-500 flex-shrink-0 ml-2'>{formatTime(chat.updatedAt)}</span>
+                              </div>
+
+                              <p className='text-sm text-gray-500 truncate mb-1'>{chatUser?.name}</p>
+                              <p className='text-xs text-gray-600 mb-1 truncate'>{chat.lastMessage || "No messages yet"}</p>
+                          </div>
+
+                        </div>
+
+                       
+
+                      </button>
+                    )
+                  })
+                }
+            </div>
+          )}
 
         </div>
     </div>
--- user/src/pages/MyListings.jsx
+++ user/src/pages/MyListings.jsx
@@ -1,9 +1,42 @@
+import { CheckCircle, DollarSign, Eye, Plus, TrendingUp } from 'lucide-react';
 import React from 'react'
+import { useSelector } from 'react-redux'
+import {useNavigate} from 'react-router-dom'
+import StatCard from '../components/StatCard';
 
 const MyListings = () => {
+
+  const {userListings,balance} =useSelector((state)=>state.listing);
+  const navigate=useNavigate();
+
+  const totalValue = userListings.reduce((sum,listing)=>sum+(listing.price || 0),0)
+  const activeListings = userListings.filter((listing)=>listing.status==='active').length
+  const soldListings = userListings.filter((listing)=>listing.status==='sold').length;
+
   return (
-    <div>
-        <h1>My listings</h1>
+    <div className='px-4 md:px-16 lg:px-24 xl:px-32 pt-8'>
+
+      {/*Header*/}
+      <div className='flex flex-col md:flex-row mditems-center items-start justify-between mb-8'>
+        <div>
+          <h1 className='font-bold text-3xl text-gray-800 mb-1'>My Listings</h1>
+          <p className='mb-1 text-gray-500'>Manage your social media account listings</p>
+        </div>
+
+        <button onClick={()=>navigate('/create-listing')} className='bg-indigo-600 hover:bg-indigo-700 rounded-lg  px-4 font-medium space-x-2 mt-4 md:mt-0 py-2 flex items-center  text-white'>
+          <Plus className='h-4 w-4'/>
+          <span>New Listing</span>
+        </button>
+      </div>
+      
+      {/*stats*/}
+      <div className='grid grid-cols-1 md:grid-cols-4 gap-6 mb-8'>
+        <StatCard title='Total Listing' value={userListings.length} icon={<Eye className='size-6 text-indigo-600'/>} color='indigo'/>
+        <StatCard title='Active Listing' value={activeListings} icon={<CheckCircle className='size-6 text-green-600'/>} color='green'/>
+        <StatCard title='Sold' value={soldListings} icon={<TrendingUp className='size-6 text-indigo-600'/>} color='indigo'/>
+        <StatCard title='Total Vakue' value={soldListings} icon={<DollarSign className='size-6 text-yellow-600'/>} color='yellow'/>      
+      </div>
+        
     </div>
   )
 }
